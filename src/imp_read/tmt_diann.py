9f3a4b244cdb8e94cd05f7852a686412936979ab
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-08-17 14:29:13 +0200

    bump version

--- src/imp_read/tmt_diann.py
+++ src/imp_read/tmt_diann.py
@@ -35,8 +35,8 @@ from .tmt_chimerys import __get_sn_for_condition
 from .tmt_spectronaut import __read_spectra
 from .tmt_spectronaut import __get_ms2_spectrum
 
-__version = "2.0.2"
-__date = "2026-04-27"
+__version = "2.1.0"
+__date = "2026-08-17"
 
 
 def __remove_ambiguous_pg(protein_table: pd.DataFrame) -> pd.DataFrame:
