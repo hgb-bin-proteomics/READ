ecc947bef3bb2f4c00f7fd65f9fc34b2268e8b57
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-08-17 16:03:04 +0200

    bump version

--- src/imp_read/tmt_spectronaut.py
+++ src/imp_read/tmt_spectronaut.py
@@ -42,8 +42,8 @@ from .tmt_chimerys import __get_sn_for_condition
 
 SPECTRONAUT_SEP = ";"
 
-__version = "2.0.3"
-__date = "2026-04-27"
+__version = "2.1.0"
+__date = "2026-08-17"
 
 
 def __remove_ambiguous_pg(protein_table: pd.DataFrame) -> pd.DataFrame:
