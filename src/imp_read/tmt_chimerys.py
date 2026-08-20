3f0a8b1f7ecdba89ab8378bf1ea5bf8dbce82e4f
Micha Johannes Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-08-20 20:39:32 +0200

    chimerys: ignore perf warnings

--- src/imp_read/tmt_chimerys.py
+++ src/imp_read/tmt_chimerys.py
@@ -1221,7 +1221,7 @@ def __annotate_chimerys_result(
     return df
 
 
-def main(argv=None) -> pd.DataFrame:
+def _main(argv=None) -> pd.DataFrame:
     parser = argparse.ArgumentParser(
         prog="tmt_chimerys.py",
         description="Calculates co-isolation purity for Chimerys DIA TMT PSMs and optionally quantifies them.",
@@ -1347,6 +1347,12 @@ def main(argv=None) -> pd.DataFrame:
             index=False,
         )
     return df
+
+
+def main(argv=None) -> pd.DataFrame:
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=pd.errors.PerformanceWarning)
+        return _main(argv)
 
 
 if __name__ == "__main__":
